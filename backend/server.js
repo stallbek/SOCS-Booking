98e1fa5d84cd348a7a06898ae8d09d90bb77bf61
Ananya Krishnakumar <ananya.krishnakv@gmail.com>
2026-04-05 00:15:51 -0400

    Updated the server setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Server is running');
+    res.send('Backend running');
 });
 
 app.listen(5000, () => {
